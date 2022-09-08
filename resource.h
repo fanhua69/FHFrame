391e37c3825910d27e001da1885dfc5b3ff83b93
David Fan <fanhua69@gmail.com>
2022-09-08 00:38:36 -0400

    Add files via upload

---
+++ resource.h
@@ -0,0 +1,71 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by FHFrame.rc
+//
+#define IDR_ICON_JLGS                   1002
+#define IDB_JLGS_BK                     1003
+#define IDB_WorkSpace_TAB               1004
+#define IDR_FHMAINFRAME                 1005
+#define IDC_MDI_TAB_CTRL_BAR            1006
+#define IDC_MDI_TAB_CTRL                1007
+#define IDB_COVER                       1008
+#define IDB_FHFRAME_DEFAULTCOVER        1009
+#define IDC_OutputList                  1010
+#define IDS_FileFormatError             1011
+#define IDS_FileCanNotRemove            1012
+#define IDS_TemplateNotFound            1013
+#define IDS_DocNotCreated               1014
+#define IDS_UnbaleCreateFile            1015
+#define IDS_ShortCutErrorWhenCreate     1016
+#define IDS_ShortCutUnbaleToFind        1017
+#define IDS_ShortCutDeletedFromStartMenu 1018
+#define IDS_ShortCutDeleteFailedFromStartMenu 1019
+#define IDS_ShortCut_RegisterTableFailedToOpen 1020
+#define IDS_ShortCutDeleteFailedOnTable 1021
+#define IDS_ShortCutDeletedFromTable    1022
+#define IDS_ShortCutCreationFailedOnTable 1023
+#define IDS_Survey                      1024
+#define IDS_Weather                     1025
+#define IDR_MENU1                       1026
+#define IDR_TOOLBAR1                    1031
+#define IDR_toolbarPowerSectionView     1033
+#define CG_IDD_PROGRESS                 3044
+#define CG_IDC_PROGDLG_PROGRESS         3058
+#define CG_IDC_PROGDLG_PERCENT          3059
+#define CG_IDC_PROGDLG_STATUS           3060
+#define IDC_PointNumber                 6009
+#define IDC_CheckPointNumber            6010
+#define IDC_Power                       6011
+#define IDC_SmoothLoop                  6012
+#define IDC_RedundantPointMigrationRate 6013
+#define IDD_DlgCurveSmoothPar           9400
+#define ID_VIEW_OUTPUTBAR               33001
+#define ID_VIEW_WORKSPACE               33002
+#define ID_WORKSPACEBAR                 33003
+#define ID_SetMainPar                   33004
+#define ID_OutputBar                    33005
+#define ID_WINDOW_NEXT_PANE             33006
+#define ID_WINDOW_PREV_PANE             33007
+#define ID_VIEW_STOP                    33008
+#define ID_VIEW_REFRESH                 33009
+#define ID_VIEW_FONTS_LARGEST           33010
+#define ID_VIEW_FONTS_LARGE             33011
+#define ID_VIEW_FONTS_MEDIUM            33012
+#define ID_VIEW_FONTS_SMALL             33013
+#define ID_VIEW_FONTS_SMALLEST          33014
+#define ID_GO_BACK                      33015
+#define ID_GO_FORWARD                   33016
+#define ID_GO_START_PAGE                33017
+#define ID_GO_SEARCH                    33018
+#define ID_BUTTONEmpty                  33019
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        1036
+#define _APS_NEXT_COMMAND_VALUE         33031
+#define _APS_NEXT_CONTROL_VALUE         1026
+#define _APS_NEXT_SYMED_VALUE           1026
+#endif
+#endif
